625cb4aacb76a2882347bf37ad95c4f75e3c3e4a
Matt Casey <matt_casey@mckinsey.com>
2014-08-09 16:28:20 -0400

    inited new services

---
+++ app/scripts/services/filters.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('appApp')
+  .service('Filters', function Filters() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ app/scripts/services/permits.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('appApp')
+  .service('Permits', function Permits() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
