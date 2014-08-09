676cb9ae95d8c6a6e94f2e506ea872577d0deb26
Matt Casey <matt_casey@mckinsey.com>
2014-08-09 17:59:48 -0400

    started populating services

--- app/scripts/services/state.js
+++ app/scripts/services/state.js
@@ -2,6 +2,15 @@
 
 angular.module('civicPandaApp')
   .service('State', function State() {
+  	var selections = {
+  		categories: [],
+    	types:      [],
+    	subtypes:   [],
+    	changes:    [],
+    	progresses: [],
+    	zones:      []
+  	}
+
     return {
     	// things user selected, overwritten on User.logIn/logOut
     	selections: selections
--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -1,7 +1,36 @@
 'use strict';
 
 angular.module('civicPandaApp')
-  .service('User', function User() {
+  .service('User', function User(State) {
+  	var emptySelections = {
+  		categories: [],
+    	types:      [],
+    	subtypes:   [],
+    	changes:    [],
+    	progresses: [],
+    	zones:      []
+  	}
+
+  	var phil = {
+  		selections: {
+	  		categories: [],
+	    	types:      [],
+	    	subtypes:   [],
+	    	changes:    [],
+	    	progresses: [],
+	    	zones:      []
+	  	}
+  	}
+
+  	function logOut() {
+  		State.selections = angular.copy(emptySelections)
+  	}
+
+  	function logIn() {
+  		// NOT angular.copy so that choices remain if log out then back in (without refresh)
+  		State.selections = phil.selections;
+  	}
+
     return {
     	// overwrites State.selections to predefined
     	logIn: logIn,
