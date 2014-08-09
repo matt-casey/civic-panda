39b02555f73722e289272bf6c12178e56d9ab56d
Matt Casey <matt_casey@mckinsey.com>
2014-08-09 08:52:19 -0400

    cleaned up placeholder yoeman content

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('civicPandaApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
