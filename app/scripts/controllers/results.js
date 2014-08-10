0ad9561fa4a628f079acc099165df55b4d20254c
Neil-Ni <tzuyangni@gmail.com>
2014-08-10 02:32:10 -0400

    added sum on done and pending permits process

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -4,12 +4,14 @@ angular.module('civicPandaApp')
   .controller('ResultsCtrl', function ($scope, Permits, Processes, User, State) {
 
   	$scope.permits = [];
+  	$scope.processSummary = {};
   	var selection = State.selection();
 
   	Permits.init().then(function(){
   		$scope.permits = Permits.getAll();
   		Processes.init().then(function(){
   			updatePermitsWithProcesses();
+  			calculateProcessSummary();
   		})
   	});
 
@@ -20,4 +22,69 @@ angular.module('civicPandaApp')
   		}
   	} 
 
+
+
+
+
+  	///////
+  	function isInFinalApproval (process) {
+  		var counter = 0;
+  		for(var x = 0; x < process.length; x ++){
+  			if(process[x].completed === true)
+  				counter ++;
+  		}
+  		return counter === 3;
+  	}
+
+  	function isInWorkingProgress (process) {
+  		var counter = 0;
+  		for(var x = 0; x < process.length; x ++){
+  			if(process[x].completed === true)
+  				counter ++;
+  		}
+  		return counter === 2;	
+  	}
+
+  	function isInWorkApproval (process) {
+  		var counter = 0;
+  		for(var x = 0; x < process.length; x ++){
+  			if(process[x].completed === true)
+  				counter ++;
+  		}
+  		return counter === 1;
+  	}
+
+  	function isInApplication (process) {
+  		var counter = 0;
+  		for(var x = 0; x < process.length; x ++){
+  			if(process[x].completed === true)
+  				counter ++;
+  		}
+  		return counter === 0;	
+  	}
+
+	function isPending (process) {
+  		var counter = 0;
+  		for(var x = 0; x < process.length; x ++){
+  			if(process[x].pending === true)
+  				counter ++;
+  		}
+  		return counter !== 0;	
+  	}
+
+  	function calculateProcessSummary(){
+  		var amountDone = 0;
+  		var amountPending = 0;
+		for(var x = 0; x < $scope.permits.length; x ++){
+			var permit = $scope.permits[x];
+			var process = permit.process.process;
+			if(isInFinalApproval && process[3].completed === true) amountDone++;
+			if(isPending(process)) amountPending++;
+		}
+		$scope.processSummary = {
+			done : amountDone,
+			pending : amountPending
+		}
+  	}
+  	///////
   });
