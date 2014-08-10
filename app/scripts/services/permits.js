93a04e4dd93171eae2d6c06302fde7508d63ff4b
Neil-Ni <tzuyangni@gmail.com>
2014-08-09 22:07:08 -0400

    added getFiltered function in permits

--- app/scripts/services/permits.js
+++ app/scripts/services/permits.js
@@ -15,8 +15,32 @@ angular.module('civicPandaApp')
 
   	/*
   		sample filter:
-  		getFiltered({ types: [1], categories: [1], types: [1], changes: [1], subtypes[1]})
+  		getFiltered({ types: [2], categories: [1], changes: [0], subtypes : [1]})
   	*/
+  	function isInCategories(permit, categories){
+  		for (var y = 0; y < categories.length; y ++)
+  			if(permit.categories.indexOf(categories[y]) > -1) return true;
+  		return false;
+  	}
+
+  	function isInTypes(permit, types){
+  		for (var y = 0; y < types.length; y ++)
+  			if(permit.types.indexOf(types[y]) > -1) return true;
+  		return false;
+  	}
+
+	function isInSubtypes(permit, subtypes){
+	  	for (var y = 0; y < subtypes.length; y ++)
+  			if(permit.subtypes.indexOf(subtypes[y]) > -1) return true;
+  		return false;
+  	}
+
+	function isInChanges(permit, changes){
+	  	for (var y = 0; y < changes.length; y ++)
+  			if(permit.changes.indexOf(changes[y]) > -1) return true;
+  		return false;
+  	}
+
   	function getFiltered(filter){
   		var filteredList = [];
   		var permit;
@@ -25,18 +49,19 @@ angular.module('civicPandaApp')
   		for (var x = 0; x < permits.length; x++){
   			permit = permits[x];
   			hasId = false;
-	  		for (var y = 0; y < filter.categories.length; y ++)
-	  			if (permit.categories.indexOf(filter.categories[y]) > -1 ) hasId = true;
 
-	  		for (var y = 0; y < filter.types.length; y ++)
-	  			if (permit.types.indexOf(filter.types[y]) > -1 ) hasId = true;
+	  		hasId = isInCategories(permit, filter.categories);
 
-	  		for (var y = 0; y < filter.changes.length; y ++)
-	  			if (permit.changes.indexOf(filter.changes[y]) > -1) hasId = true;
+	  		if(hasId === true){
+	  			hasId = isInTypes(permit, filter.types);
+	  		}
 
-	  		for (var y = 0; y < filter.subtypes.length; y ++)
-	  			if (permit.subtypes.indexOf(filter.subtypes[y]) > -1 ) hasId = true;
+	  		if(hasId === true){
+	  			hasId = isInSubtypes(permit, filter.subtypes)
+	  		}
 
+	  		if(isInChanges(permit, filter.changes) === true) hasId = true;
+	  		
 	  		if (hasId === true) filteredList.push(permit);
 	  	}
 	  	console.log(filteredList);
