00ce4b950ff8398f2b3e6238035d90b8d7ae3192
Neil-Ni <tzuyangni@gmail.com>
2014-08-10 11:50:50 -0400

    added web link infor for each process for permit application

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -57,6 +57,8 @@ angular.module('civicPandaApp')
         for(var x = 0; x < $scope.permits.length; x ++){
             var permit = $scope.permits[x];
             permit.process = emptyProcess;
+            if(permit.process)
+                permit.process.process = addWebInfo(permit.process.process);
             permit.newPermit = true;
         }
     }
@@ -68,9 +70,39 @@ angular.module('civicPandaApp')
 	
 		for(var x = 0; x < $scope.permits.length; x ++){
 			var permit = $scope.permits[x];
-			permit.process = Processes.get(permit.id);
+            var process = Processes.get(permit.id) 
+            if(process)
+                process.process = addWebInfo(process.process);
+            permit.process = process;
 		}
 	} 
+
+    function addWebInfo(process){
+        var p = process;
+        if( p[0].completed === false ){
+            p[0].web = {
+                name: "Start",
+                linke: "www.cityofboston.gov/isd/building/permits.asp"
+            }
+        }
+        if( p[1].completed === false && p[0].completed === true){
+            p[0].web = null;
+            p[1].web = {
+                name: "Start",
+                link: "www.cityofboston.gov/isd/building/permits.asp"
+            }
+        }
+        if( p[2].completed === false && p[1].completed === true && p[0].completed === true){
+            p[0].web = null;
+            p[1].web = null;
+            p[2].web = {
+                name: "Start",
+                link: "www.cityofboston.gov/isd/building/permits.asp"
+            }
+        }
+        return p;
+    }
+
 
 	///////
 	function isInFinalApproval (process) {
