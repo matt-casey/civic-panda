bcd13432ab77d11f5e22b210ba976c65753243ab
Neil-Ni <tzuyangni@gmail.com>
2014-08-10 01:55:18 -0400

    setup results controller

--- app/scripts/services/processes.js
+++ app/scripts/services/processes.js
@@ -1,13 +1,24 @@
 'use strict';
 
 angular.module('civicPandaApp')
-  .service('Processes', function Processes($http) {
+  .service('Processes', function Processes($http, $q) {
 
   	var processes = [];
 
-    $http.get('data/processes.json').success(function(response) {
-  		processes = response;
-    });
+    init();
+    
+    function init() {
+      var deferred = $q.defer();
+      $http.get('data/processes.json')
+        .success(function(response) {
+          processes = response;
+          deferred.resolve();
+        })
+      .error(function(){
+        deferred.reject();
+      });
+      return deferred.promise;
+    }
 
   	function get(id){
       for (var y = 0; y < processes.length; y ++)
@@ -15,6 +26,7 @@ angular.module('civicPandaApp')
   	}
 
     return {
+      init: init,
     	get: get
     }
   });
